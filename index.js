047f05777ae1a1fccd0c4d4c6c4c7f6294e65bde
kugel-qt <113015908+kugel-qt@users.noreply.github.com>
2022-09-08 21:00:51 +0200

    Basic working generator
    ability to filter characters not implemented yet

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
-let characters = {
-    letters: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-    numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    symbols: ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
-        "/"],
-    allowed: []
-}
+let characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+        "/"]
 const generateBtn = document.getElementById("generate-btn")
 let password = { one: "", two: "" }
 
 generateBtn.addEventListener("click", function () {
-    allowedCharacters(); passwordGenerator(); test()
+    passwordGenerator()
 })
 
 function passwordGenerator() {
     password.one = ""
     password.two = ""
     for (let i = 0; i < 30; i++) {
-        let j = Math.floor(Math.random() * characters.allowed.length)
+        let j = Math.floor(Math.random() * characters.length)
         if (i < 15) {
-            password.one += characters.allowed[j]
+            password.one += characters[j]
             document.querySelector("#password-one").textContent = password.one
         } else {
-            password.two += characters.allowed[j]
+            password.two += characters[j]
             document.querySelector("#password-two").textContent = password.two
         }
     }
-}
-
-function allowedCharacters() {
-    characters.allowed = []
-    characters.allowed.push(characters.letters)
-    if (document.querySelector("#symbols-chk").checked)
-    characters.allowed.push(characters.symbols)
-    console.log("Symbols checked");
-    if (document.querySelector("#num-chk").checked)
-    characters.allowed.push(characters.numbers)
-    console.log("Numbers checked");
-}
-
-function test() {
-    document.querySelector('#test').textContent = characters.allowed.length
 }
