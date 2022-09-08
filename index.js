61f407732cfd36bebd7fc212a315e934b4eb161e
kugel-qt <113015908+kugel-qt@users.noreply.github.com>
2022-09-08 21:35:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-let characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] 
+const symbols = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
         "/"]
+let allowedCharacters = []
 const generateBtn = document.getElementById("generate-btn")
 let password = { one: "", two: "" }
 
 generateBtn.addEventListener("click", function () {
-    passwordGenerator()
+    characterSelector(); passwordGenerator()
 })
 
 function passwordGenerator() {
@@ -20,4 +23,11 @@ function passwordGenerator() {
             document.querySelector("#password-two").textContent = password.two
         }
     }
+}
+
+function characterSelector() {
+    allowedCharacters = []
+    allowedCharacters.push(characters)
+    if (document.querySelector('#symbols-chk').checked) {allowedCharacters.push(symbols)}
+    if (document.querySelector('#num-chk').checked) {allowedCharacters.push(numbers)}
 }
